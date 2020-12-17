e73984eecbab1ea51181c1b0d6912ed5341419af
Lê Xuân Mạnh <53442492+lemanh99@users.noreply.github.com>
2020-12-17 21:11:57 +0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,14 @@
-# def application(env, start_response):
-#     import sys
-#     import socket
-#     from datetime import date
-#     from string import Template
+def application(env, start_response):
+    import sys
+    import socket
+    from datetime import date
+    from string import Template
 
-#     with open ("ROOT/run.py", "r") as index_file:
-#          data=index_file.read().replace('\n', '')
-#          index_object = Template(data)
-#          index_formated=index_object.substitute(mydate=date.today().year, hostname=socket.gethostname())
-#          if sys.version.split(' ')[0].split('.')[0] == '3':
-#             index_formated = index_formated.encode('utf-8')
-#     start_response('200 OK', [('Content-Type','text/html')])
-#     return [ index_formated  ]
-from shop import app
-app.run(debug=True)
+    with open ("ROOT/run.py", "r") as index_file:
+         data=index_file.read().replace('\n', '')
+         index_object = Template(data)
+         index_formated=index_object.substitute(mydate=date.today().year, hostname=socket.gethostname())
+         if sys.version.split(' ')[0].split('.')[0] == '3':
+            index_formated = index_formated.encode('utf-8')
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [ index_formated  ]
